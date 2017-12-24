3b7c46c3d66cbae9d903b0268e927ae3c88982dd
kanehara <kanehara@umich.edu>
2017-12-23 18:13:40 -0600

    Adds initial tests

---
+++ server/test/core.js
@@ -0,0 +1,30 @@
+const request = require('supertest')
+const app = require('../app')
+
+module.exports = test => {
+  test('healthz check', t => {
+    request(app)
+      .get('/healthz')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .end((err, res) => {
+        const expectedBody = 'Oooh weee look at me!'
+        const actualBody = res.text
+        t.error(err, 'No error')
+        t.equal(actualBody, expectedBody, 'Healthz check')
+        t.end()
+      })
+  })
+
+  test('404 on nonexistant URL', (t) => {
+    request(app)
+      .get('/GETShouldFailOnRandomURL')
+      .expect(404)
+      .expect('Content-Type', /text\/html/)
+      .end((err, res) => {
+        t.error(err, 'No error')
+        t.notEqual(res.text.indexOf('Cannot GET /GETShouldFailOnRandomURL'), -1)
+        t.end()
+      })
+  })
+}
---
+++ server/test/main.js
@@ -0,0 +1,4 @@
+const test = require('tape')
+const core = require('./core')
+
+core(test)
